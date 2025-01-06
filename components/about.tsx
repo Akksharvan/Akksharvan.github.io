b3e8d8117e2fd05dfb5ea1540c3f14f200ade1cd
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2025-01-06 02:20:59 -0500

    Work in Progress

--- components/about.tsx
+++ components/about.tsx
@@ -17,25 +17,8 @@ export default function About() {
             <SectionHeading>About Me</SectionHeading>
 
             <p className = "mb-3">
-                I'm a student studying Computer Science & Applied Mathematics at UNC Chapel Hill. I'm passionate about software development, machine learning, and data science.I am deeply passionate about the intersection of machine learning and software development. <span className = "italic">My favorite part of programming</span> is building innovative solutions that make a real impact. I <span className = "underline">love</span> tackling complex challenges, from developing ensemble ML models for stock prediction to implementing transformers from scratch. I'm currently looking for internships and full-time opportunities in software development, machine learning, and data science. Feel free to reach out to me if you have any opportunities or projects you'd like to work on together.
+                I&apos;m a student studying Computer Science & Applied Mathematics at UNC Chapel Hill. I&apos;m passionate about software development, machine learning, and data science. I am deeply passionate about the intersection of machine learning and software development. <span className = "italic">My favorite part of programming</span> is building innovative solutions that make a real impact. I <span className = "underline">love</span> tackling complex challenges, from developing ensemble ML models for stock prediction to implementing transformers from scratch. I&apos;m currently looking for internships and full-time opportunities in software development, machine learning, and data science. Feel free to reach out to me if you have any opportunities or projects you&apos;d like to work on together.
             </p>
         </motion.section>
     )
 }
-
-// export default function About() {
-//   const { ref } = useSectionInView("About");
-
-//   return (
-//     <motion.section
-//       ref = {ref}
-//       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-//       initial = {{ opacity: 0, y: 100 }}
-//       animate = {{ opacity: 1, y: 0 }}
-//       transition = {{ delay: 0.175 }}
-//       id = "about"
-//     >
-//       <SectionHeading>About Me</SectionHeading>
-//     </motion.section>
-//   );
-// }
--- components/intro.tsx
+++ components/intro.tsx
@@ -57,7 +57,7 @@ export default function Intro() {
                     className = "mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
                     initial =  {{ opacity: 0, y: 100 }}
                     animate = {{ opacity: 1, y: 0 }}>
-                    <span className = "font-bold"> Hello, I'm Akksharvan. </span> I'm a student studying <span className = "font-bold"> Computer Science & Applied Mathematics </span> at <span className = "font-bold">UNC Chapel Hill</span>.
+                    <span className = "font-bold"> Hello, I&apos;m Akksharvan. </span> I&apos;m a student studying <span className = "font-bold"> Computer Science & Applied Mathematics </span> at <span className = "font-bold">UNC Chapel Hill</span>.
                 </motion.h1>
 
                 <motion.div 
