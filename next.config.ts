6d0fd81d3c6493e11591ff5135b1097d558d021f
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2025-01-06 02:52:31 -0500

    Work in Progress

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "build",
-  images: { unoptimized: true }
 };
 
 export default nextConfig;
